06eab28c54f18563213d6a2609c81ae44f74e5d7
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-09-22 09:23:26 -0300

    * printing to console as well as to the log

--- sweethome/errors.py
+++ sweethome/errors.py
@@ -6,6 +6,8 @@ logger.setLevel(logging.DEBUG)
 
 class MockLoggerFile:
     def write(self, s):
+        for line in s.splitlines():
+            print("DEBUG: ", line)
         logger.debug(s)
 
 logger_file = MockLoggerFile()
