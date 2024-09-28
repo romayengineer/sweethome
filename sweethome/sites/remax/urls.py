1be1488d8ffc315a14e8fbe2b997cc86362b2a70
Maximiliano Romay Figueroa <romayengineer@gmail.com>
2024-09-28 15:38:31 -0300

    * new function to get departments page by page index

--- sweethome/sites/remax/urls.py
+++ sweethome/sites/remax/urls.py
@@ -27,22 +27,27 @@ def unparse(url: str = None, query: Dict[str, str] = None) -> str:
     )
 
 
+def get_department_page(page_index: int) -> str:
+    assert page_index >= 0
+    return unparse(
+        url="/listings/rent",
+        query={
+            "page": str(page_index),
+            "pageSize": "24",
+            "sort": "-createdAt",
+            "in:operationId": "2",
+            "in:eStageId": "0,1,2,3,4",
+            "in:typeId": "1,2,3,4,5,6,7,8",
+            "filterCount": "1",
+            "viewMode": "listViewMode",
+        },
+    )
+
+
 home = unparse(url="")
 
 login = unparse(url="iniciar-sesion")
 
 signup = unparse(url="/registro")
 
-departments_all = unparse(
-    url="/listings/rent",
-    query={
-        "page": "0",
-        "pageSize": "24",
-        "sort": "-createdAt",
-        "in:operationId": "2",
-        "in:eStageId": "0,1,2,3,4",
-        "in:typeId": "1,2,3,4,5,6,7,8",
-        "filterCount": "1",
-        "viewMode": "listViewMode",
-    },
-)
+departments_all = get_department_page(page_index=0)
